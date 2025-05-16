ec661d590059583ce25aa629f759445f3f771c41
madisondegrezia <madisondegrezia26@gmail.com>
2025-05-16 15:15:30 -0400

    Created endpoint to get array of restaurant objects

--- src/app.js
+++ src/app.js
@@ -4,5 +4,9 @@ const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below:
+app.get("/restaurants", async (req, res) => {
+  const restaurants = await Restaurant.findAll({});
+  res.json(restaurants);
+});
 
 module.exports = app;
