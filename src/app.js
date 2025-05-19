c014f13b938e99637ca88b183c9f922f5670e100
madisondegrezia <madisondegrezia26@gmail.com>
2025-05-18 21:27:13 -0400

    returns restaurant requested by id

--- src/app.js
+++ src/app.js
@@ -4,9 +4,17 @@ const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below:
+
+// get all restaurants
 app.get("/restaurants", async (req, res) => {
   const restaurants = await Restaurant.findAll({});
   res.json(restaurants);
+});
+
+// get restaurant by specific id
+app.get("/restaurants/:id", async (req, res) => {
+  const restaurant = await Restaurant.findByPk(req.params.id);
+  res.json(restaurant);
 });
 
 module.exports = app;
