00a311d652e05a0f490c59e09a89a5372951b326
madisondegrezia <madisondegrezia26@gmail.com>
2025-05-18 21:48:12 -0400

    Created Express route handlers that create, update, and delete resources from restaurant's database

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below:
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // get all restaurants
 app.get("/restaurants", async (req, res) => {
   const restaurants = await Restaurant.findAll({});
@@ -15,6 +18,25 @@ app.get("/restaurants", async (req, res) => {
 app.get("/restaurants/:id", async (req, res) => {
   const restaurant = await Restaurant.findByPk(req.params.id);
   res.json(restaurant);
+});
+
+app.post("/restaurants", async (req, res) => {
+  const newRestaurant = await Restaurant.create(req.body);
+  res.json(newRestaurant);
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+  const updatedRestaurant = await Restaurant.update(req.body, {
+    where: { id: req.params.id },
+  });
+  res.json(updatedRestaurant);
+});
+
+app.delete("/restaurants/:id", async (req, res) => {
+  const deletedRestaurant = await Restaurant.destroy({
+    where: { id: req.params.id },
+  });
+  res.json(deletedRestaurant);
 });
 
 module.exports = app;
